0993ce6eef558cba91df6228367fe929e3868a5e
moth1995 <marcos-flores@live.com.ar>
2022-07-01 01:28:36 -0300

    dinamically create obj at the same folder as bin file source

--- model_tool.py
+++ model_tool.py
@@ -1,13 +1,14 @@
 from file_structure import FacePCModel, Container, unzlib_it, file_read
+from pathlib import Path
 
-def create_obj(pes_model:FacePCModel, bin_source:str):
-    with open ("Messi.obj","w") as obj_file:
+def create_obj(pes_model:FacePCModel, folder:str, filename:str):
+    with open (f"{folder}/{filename}.obj","w") as obj_file:
         obj_file.writelines("# PES/WE/JL Model Tool\n")
-        obj_file.writelines(f"# OBJ Model {bin_source}\n")
+        obj_file.writelines(f"# OBJ Model {filename}\n")
         obj_file.writelines("\n")
-        obj_file.writelines("mtllib Messi.mtl\n")
+        obj_file.writelines(f"mtllib {filename}.mtl\n")
         obj_file.writelines("\n")
-        obj_file.writelines("o Messi_face \n")
+        obj_file.writelines(f"o {filename} \n")
         obj_file.writelines("\n")
         obj_file.writelines(f"# Vertices {len(pes_model.vertex_list)}\n")
         for i in range(len(pes_model.vertex_list)):
@@ -34,22 +35,31 @@ def create_obj(pes_model:FacePCModel, bin_source:str):
             + f"{pes_model.polygonal_faces_list[i].i3}/{pes_model.polygonal_faces_list[i].i3}/{pes_model.polygonal_faces_list[i].i3}\n"
             )
 
-def create_mtl():
-    with open('Messi.mtl',"w") as mtl_file:
+def create_mtl(folder:str, filename:str):
+    with open(f'{folder}/{filename}.mtl',"w") as mtl_file:
         mtl_file.writelines("newmtl material1 \n")
-        mtl_file.writelines("\tmap_Kd Messi.png \n")
+        mtl_file.writelines(f"\tmap_Kd {filename}.png \n")
+
+def get_container(unzlibed_file:bytearray):
+    return Container(unzlibed_file)
 
 def get_face_hair_model(file_location:str):
     bin_file = file_read(file_location)
     decompress_bin_file = unzlib_it(bin_file[32:])
-    file_ctn = Container(decompress_bin_file)
+    file_ctn = get_container(decompress_bin_file)
     return FacePCModel(file_ctn.files[0])
 
 def main():
-    bin_location = "Messi.bin"
-    face_pc_model = get_face_hair_model(bin_location)
-    create_obj(face_pc_model, bin_location)
-    create_mtl()
+    # from a string we get a Path object and from that we get the values that we need
+    bin_location = Path("./Beckham-models/pc/face-unnamed_2009.bin")
+    bin_full_path = str(bin_location.resolve())
+    bin_filename = bin_location.stem
+    bin_folder_location = str(bin_location.parent)
+    
+    model = get_face_hair_model(bin_full_path)
+    
+    create_obj(model, bin_folder_location, bin_filename)
+    create_mtl(bin_folder_location, bin_filename)
 
 if __name__ == "__main__":
 	main()
